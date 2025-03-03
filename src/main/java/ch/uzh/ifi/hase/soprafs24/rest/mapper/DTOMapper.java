eced5cff8eb2be73a8f6841a3791c19ef9abc80e
ermin-mumic <ermin.mumic@uzh.ch>
2025-03-03 09:25:31 +0100

    Deployment

--- src/main/java/ch/uzh/ifi/hase/soprafs24/rest/mapper/DTOMapper.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/rest/mapper/DTOMapper.java
@@ -26,7 +26,6 @@ public interface DTOMapper {
   @Mapping(source = "name", target = "name")
   @Mapping(source = "username", target = "username")
   @Mapping(source = "password", target = "password")
-  @Mapping(source = "birthday", target = "birthday")
   User convertUserPostDTOtoEntity(UserPostDTO userPostDTO);
 
   @Mapping(source = "id", target = "id")
